af450011a67c5a12681f7fd567925ac0041258df
Ramon Gene <rgene@rccl.com>
2024-01-15 16:52:47 -0500

    Finished demo 3

---
+++ e2e/countries.spec.ts
@@ -0,0 +1,167 @@
+import { test, expect } from 'next/experimental/testmode/playwright';
+import { createJsonResponse, createJsonResponseWithStatus } from './test-utils';
+import { countries } from './mocks/countries';
+import { countryDetails } from './mocks/countryDetails';
+
+test.describe('Countries page content', () => {
+    test('has correct content', async ({ page, next}) => {
+        next.onFetch(async (request) => { 
+            return createJsonResponse({
+                data: { countries },
+            });            
+        });          
+
+        await page.goto('/');
+        await expect(page).toHaveTitle('Countries of the world');
+        await expect(page.getByTestId('country-label')).toHaveText('Country');
+        await page.getByTestId('country-select').selectOption({label: 'Test country 1'});
+    });
+});
+
+test.describe('Country selection', () => {
+    test('country selection populates the correct info', async ({ page, next}) => {
+        let requestBody: { variables: object} | null = null;
+        next.onFetch(async (request) => { 
+            const testId = request.headers.get("data-testid");  
+            if (testId==="getCountries") {                      
+                return createJsonResponse({
+                    data: { countries },
+                });    
+            }   
+            
+            if (testId==="getCountryDetails") { 
+                requestBody = await request.json();                     
+                return createJsonResponse({
+                    data: { country : countryDetails },
+                });    
+            } 
+            
+            return 'continue';
+        });          
+
+        await page.goto('/');
+
+        await expect(page.getByTestId('capital-label')).not.toBeVisible();
+        await expect(page.getByTestId('error-label')).not.toBeVisible();
+
+        await page.getByTestId('country-select').selectOption('C2');
+        await page.getByTestId('submit-button').click();
+        expect((requestBody as any).variables.id).toBe('C2');
+
+        await expect(page.getByTestId('capital-value-label')).toHaveText('test capital');
+        await expect(page.getByTestId('currency-value-label')).toHaveText('test currency');
+    });
+
+    test('shows error when graph call fails', async ({ page, next}) => {
+
+        next.onFetch(async (request) => { 
+            const testId = request.headers.get("data-testid");  
+            if (testId==="getCountries") {                      
+                return createJsonResponse({
+                    data: { countries },
+                });    
+            }   
+            
+            if (testId==="getCountryDetails") { 
+                return createJsonResponseWithStatus({
+                    data: { country : countryDetails },
+                }, 500);    
+            } 
+            
+            return 'continue';
+        });          
+
+        await page.goto('/');
+
+        await page.getByTestId('country-select').selectOption('C2');
+        await page.getByTestId('submit-button').click();
+        await expect(page.getByTestId('error-label')).toBeVisible();
+
+    });   
+    
+    test('caches graph call', async ({ page, next}) => {
+        let numberOfGraphInvocations = 0;
+        
+        next.onFetch(async (request) => { 
+            const testId = request.headers.get("data-testid");  
+            if (testId==="getCountries") {                      
+                return createJsonResponse({
+                    data: { countries },
+                });    
+            }   
+            
+            if (testId==="getCountryDetails") { 
+                numberOfGraphInvocations++;     
+                return createJsonResponse({
+                    data: { country : countryDetails },
+                });    
+            } 
+            
+            return 'continue';
+        });          
+
+        await page.goto('/');
+
+        await page.getByTestId('country-select').selectOption('C2');
+        await page.getByTestId('submit-button').click();
+        await page.getByTestId('submit-button').click();
+        expect(numberOfGraphInvocations).toBe(1);
+
+    });
+    
+    test('shows error when graph brings invalid data', async ({ page, next}) => {
+
+        next.onFetch(async (request) => { 
+            const testId = request.headers.get("data-testid");  
+            if (testId==="getCountries") {                      
+                return createJsonResponse({
+                    data: { countries },
+                });    
+            }   
+            
+            if (testId==="getCountryDetails") { 
+                return createJsonResponse({
+                    data: { },
+                });    
+            } 
+            
+            return 'continue';
+        });          
+
+        await page.goto('/');
+
+        await page.getByTestId('country-select').selectOption('C2');
+        await page.getByTestId('submit-button').click();
+        await expect(page.getByTestId('error-label')).toBeVisible();
+
+    });    
+
+    test('defaults to first country', async ({ page, next}) => {
+        let requestBody: { variables: object} | null = null;
+        next.onFetch(async (request) => { 
+            const testId = request.headers.get("data-testid");  
+            if (testId==="getCountries") {                      
+                return createJsonResponse({
+                    data: { countries },
+                });    
+            }   
+            
+            if (testId==="getCountryDetails") { 
+                requestBody = await request.json();
+                return createJsonResponse({
+                    data: { country : countryDetails },
+                });    
+            } 
+            
+            return 'continue';
+        });          
+
+        await page.goto('/');
+
+        await page.getByTestId('submit-button').click();
+        expect((requestBody as any).variables.id).toBe('C1');
+
+
+    });    
+});
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import type { Metadata } from 'next'
+import { Countries } from '../components/countries'
+import { fetchFromCountriesGraph } from '../lib/fetcher';
+import Country from '../models/country';
 
  
 export const metadata: Metadata = {
-
+  title: 'Countries of the world'
 }
 
 export default async function Page() {
-  
- 
+  const response  = await fetchFromCountriesGraph({
+    query: '{ countries { name, code } }',
+  }, 
+  'getCountries').then((res) => res.json());
 
-  return ( <></>
+  const countries : Country[] = response.data.countries; 
+ 
+  return ( <div>
+    <Countries countryListItems={countries}></Countries>
+    </div>
   )
 }
