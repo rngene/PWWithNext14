438031a11daef9ee51bb6ca433f0bfdc5ea5d231
Ramon Gene <rgene@rccl.com>
2024-01-18 18:52:54 -0500

    Added validation of graph call parameter

--- e2e/countries.spec.ts
+++ e2e/countries.spec.ts
@@ -20,6 +20,7 @@ test.describe('Countries page content', () =>{
 
 test.describe('Country selection', () =>{
     test('country dropdowns selects and populates info correctly', async({ page ,next}) => {
+        let requestBody: { variables: object} | null = null;
         next.onFetch(async (request) => { 
             const testId = request.headers.get("data-testid");
             if (testId==="getCountries") {
@@ -28,6 +29,7 @@ test.describe('Country selection', () =>{
                 });            
             }
             if (testId==="getCountryDetails") {
+                requestBody = await request.json();
                 return createJsonResponse({
                     data: { country: countryDetails },
                 });            
@@ -37,6 +39,7 @@ test.describe('Country selection', () =>{
         await page.goto('/');
         await page.getByTestId('country-select').selectOption('C2');
         await page.getByTestId('submit-button').click();
+        expect((requestBody as any).variables.id).toBe('C2');
 
         await expect(page.getByTestId('capital-value-label')).toHaveText('test capital');
         await expect(page.getByTestId('currency-value-label')).toHaveText('test currency');
