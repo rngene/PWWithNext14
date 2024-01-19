dea70a57b559050b0def5bf1d8adb841e831ec1b
Ramon Gene <rgene@rccl.com>
2024-01-19 14:28:59 -0500

    Populated dropdown

--- e2e/countries.spec.ts
+++ e2e/countries.spec.ts
@@ -1,10 +1,17 @@
 import { test, expect } from 'next/experimental/testmode/playwright';
+import { createJsonResponse } from './test-utils';
+import { countries } from './mocks/countries';
 
 test.describe('Countries page content', () => {
-    test('has correct content', async ({page}) => {
+    test('has correct content', async ({page, next}) => {
+        next.onFetch(async (request) => { 
+            return createJsonResponse({
+                data: { countries },
+            });            
+        });            
         await page.goto('/');
         await expect(page).toHaveTitle('Countries of the World');
         await expect(page.getByTestId('country-label')).toHaveText('Country');
-
+        await page.getByTestId('country-select').selectOption({label: 'Test country 1'});
     })
 })
